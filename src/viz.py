bbe643cef66bb05edc394721ed593a87014cb20a
Jialei Ni <jialei.ni@berkeley.edu>
2026-07-28 14:25:43 -0400

    fix: import Patch in viz.py

--- src/viz.py
+++ src/viz.py
@@ -12,6 +12,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import TwoSlopeNorm
+from matplotlib.patches import Patch
 
 from src.perf import drawdown
 
